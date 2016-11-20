eb84ba656f44f3d1be542e0716c0f3d1abaf2455
mdm <marcmigge@gmx.net>
2016-11-20 16:03:49 +0100

    Use fixed timestep for animation

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ int main(int argc, char** argv)
             Window window("Paint The Town Pink!");
             SDL_Event event;
             Renderer renderer;
+
+            // test scene
             Quad* quad = new Quad(200, 200, 8);
             glm::mat4 translate = glm::translate(glm::vec3(100.0f, 100.0f, 0.0f));
             glm::mat4 rotate = glm::rotate(3.1415926f / 4, glm::vec3(0.0f, 0.0f, 1.0f));
@@ -28,26 +30,35 @@ int main(int argc, char** argv)
             renderer.addQuad(quad);//*/
             bool quit = false;
             float angleRadians = 3.1415926f / 4;
+
+            uint32_t previousTimestamp = SDL_GetTicks();
+            uint32_t lag = 0;
             while(!quit)
             {
+                uint32_t currentTimestamp = SDL_GetTicks();
+                lag += currentTimestamp - previousTimestamp;
+                previousTimestamp = currentTimestamp;
+
                 SDL_PollEvent(&event);
                 if(event.type == SDL_QUIT)
                 {
                     quit = true;
                 }
+
+                while(lag >= 16) {
+                    angleRadians += 0.01f;
+                    if(angleRadians > 2 * 3.1415926f)
+                    {
+                        angleRadians = 0.0f;
+                    }
+                    rotate = glm::rotate(angleRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+                    quad->setTransform(translate * rotate);
+
+                    lag -= 16;
+                }
+
                 renderer.renderFrame();
                 window.Swap();
-                angleRadians += 0.001f;
-                if(angleRadians > 2 * 3.1415926f)
-                {
-                    angleRadians = 0.0f;
-                    //quit = true;
-                }
-                rotate = glm::rotate(angleRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-                quad->setTransform(translate * rotate);
-
-                //SDL_Delay(8000);
-                //quit = true;
             }
         }
         SDL_Quit();
